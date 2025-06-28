07b77d2e576584624dd73a5f01cb0f769fc9aa53
Dotan Veretzky <dotanvg@gmail.com>
2025-06-28 20:28:45 +0300

    Add menu flow and placeholder settings

--- Assets/Scripts/Controllers/MainMenuController.cs
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Displays a simple main menu with buttons for navigating to the
+// Game and Settings scenes.
+
 public class MainMenuController : MonoBehaviour
 {
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100));
+        if (GUILayout.Button("New Game"))
+        {
+            StartGame();
+        }
+        if (GUILayout.Button("Settings"))
+        {
+            OpenSettings();
+        }
+        GUILayout.EndArea();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
